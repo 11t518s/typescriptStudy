76b45f8d3108c122410da13dafee54a8920c6254
11t518s <11t518s20161759@gmail.com>
2021-12-23 20:50:03 +0900

    fix conflict

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+var Car = /** @class */ (function () {
+    function Car() {
+        this.engine = 1;
+    }
+    return Car;
+}());
--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
 console.log("hello world");
+class Car {
+  engine: number;
+  constructor() {
+    this.engine = 1;
+  }
+}
