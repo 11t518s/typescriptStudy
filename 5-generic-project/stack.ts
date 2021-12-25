c7decbd3275009faffe35f77548a5021a6032847
11t518s <11t518s20161759@gmail.com>
2021-12-25 10:39:33 +0900

    ~8강

--- 5-generic-project/stack.ts
+++ 5-generic-project/stack.ts
@@ -1,57 +1,126 @@
 ////////////////////////////////////////////////////////////
+{
+  interface IStack<T> {
+    readonly size: number;
 
-interface Stack {
-  readonly size: number;
+    push(value: T): void;
 
-  push(value: string): void;
+    pop(): T;
+  }
 
-  pop(): string;
+  type TStackNode<T> = {
+    readonly value: T;
+    readonly next?: TStackNode<T>;
+  };
+
+  class StackImpl<T> implements IStack<T> {
+    private head?: TStackNode<T>;
+
+    constructor(private capacity: number) {}
+
+    private _size: number = 0;
+
+    get size() {
+      return this._size;
+    }
+
+    push(value: T) {
+      if (this.size === this.capacity) {
+        throw new Error("Stack is full");
+      }
+      const node: TStackNode<T> = {
+        value,
+        next: this.head,
+      };
+      this.head = node;
+      this._size++;
+    }
+
+    pop(): T {
+      if (this.head == null) {
+        throw new Error("IStack is empty!");
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
+  }
+
+  const stack = new StackImpl(5);
+  stack.push("bong1");
+  stack.push("123");
+  stack.push("4324234");
+
+  while (stack.size !== 0) {
+    console.log(stack.pop());
+  }
 }
 
-type StackNode = {
-  readonly value: string;
-  readonly next?: StackNode;
-};
+////
+{
+  interface IStack<T> {
+    readonly size: number;
 
-class StackImpl implements Stack {
-  private head?: StackNode;
+    push(value: T): void;
 
-  constructor(private capacity: number) {}
-
-  private _size: number = 0;
-
-  get size() {
-    return this._size;
+    pop(): T;
   }
 
-  push(value: string) {
-    if (this.size === this.capacity) {
-      throw new Error("Stack is full");
+  type TStackNode<T> = {
+    readonly value: T;
+    readonly next?: TStackNode<T>;
+  };
+
+  class StackImpl<T> implements IStack<T> {
+    private head?: TStackNode<T>;
+
+    constructor(private capacity: number) {}
+
+    private _size: number = 0;
+
+    get size() {
+      return this._size;
     }
-    const node: StackNode = {
-      value,
-      next: this.head,
-    };
-    this.head = node;
-    this._size++;
-  }
 
-  pop(): string {
-    if (this.head == null) {
-      throw new Error("Stack is empty!");
+    push(value: T) {
+      if (this.size === this.capacity) {
+        throw new Error("Stack is full");
+      }
+      const node = {
+        value,
+        next: this.head,
+      };
+      this.head = node;
+      this._size++;
     }
-    const node = this.head;
-    this.head = node.next;
-    this._size--;
-    return node.value;
+
+    pop(): T {
+      if (this.head == null) {
+        throw new Error("Stack is empty!");
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
+    }
   }
-}
 
-const stack = new StackImpl(5);
-stack.push("bong1");
-stack.push("123");
-stack.push("4324234");
+  const stack = new StackImpl<string>(5);
+  stack.push("bong1");
+  stack.push("123");
+  stack.push("4324234");
 
-while (stack.size !== 0) {
-  console.log(stack.pop());
+  while (stack.size !== 0) {
+    console.log(stack.pop());
+  }
+
+  const stack2 = new StackImpl<number>(5);
+  stack2.push(1);
+  stack2.push(123);
+  stack2.push(4324234);
+
+  while (stack2.size !== 0) {
+    console.log(stack2.pop());
+  }
 }
