588b36fdf2b01e259bed7ea2814b0705c3492485
bongsu <11t518s20161759@gmail.com>
2021-12-27 17:59:49 +0900

    ~11.3

---
+++ 10-javascript/10-1-prototype.js
@@ -0,0 +1,36 @@
+const x = {};
+const y = {};
+console.log(x);
+console.log(y);
+console.log(x.toString());
+console.log(x.__proto__ === y.__proto__);
+
+const array = [];
+console.log(array);
+
+function CoffeeMachine(beans) {
+  this.beans = beans;
+  // instance member level
+  // this.makeCoffee = (shots) => {
+  //   console.log("making");
+  // };
+}
+
+// prototype level
+CoffeeMachine.prototype.makeCoffee = function (shots) {
+  console.log("making");
+};
+const machine1 = new CoffeeMachine(10);
+const machine2 = new CoffeeMachine(20);
+
+console.log(machine1);
+console.log(machine2);
+
+function LatteMachine(milk) {
+  this.milk = milk;
+}
+LatteMachine.prototype = Object.create(CoffeeMachine.prototype);
+LatteMachine.prototype.makeCoffee();
+const latteMachine = new LatteMachine(123);
+latteMachine.makeCoffee();
+console.log(latteMachine);
