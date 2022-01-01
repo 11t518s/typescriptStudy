6ef7c67005a369e3cca1a2b9b4f3de3a2eb5ac51
11t518s <11t518s20161759@gmail.com>
2022-01-01 20:17:43 +0900

    image componenets

--- 12-motion/dist/app.js
+++ 12-motion/dist/app.js
@@ -1,8 +1,11 @@
 import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/item/image.js";
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
+        const image = new ImageComponent("Image Title", "http://picsum.photos/600/300");
+        image.attachTo(appRoot, "beforeend");
     }
 }
 new App(document.querySelector(".document"));
--- 12-motion/src/app.ts
+++ 12-motion/src/app.ts
@@ -1,4 +1,5 @@
 import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/item/image.js";
 
 class App {
   private readonly page: PageComponent;
@@ -6,6 +7,12 @@ class App {
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Title",
+      "http://picsum.photos/600/300"
+    );
+    image.attachTo(appRoot, "beforeend");
   }
 }
 
